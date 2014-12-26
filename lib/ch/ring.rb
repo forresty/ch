621197775147515dd53d7b55b3564809ff0f9727
Forrest Ye <afu@forresty.com>
2014-12-26 16:52:13 +0800

    hash_for_key

--- lib/ch/ring.rb
+++ lib/ch/ring.rb
@@ -1,11 +1,17 @@
+require "digest/sha1"
+
 module ConsistentHashing
   class Ring
+    def initialize
+    end
+
+    # a node can be inserted into multiple positions
     def add_node(node, position=nil)
       raise 'not implemented yet'
     end
 
-    def position_for_key(key)
-      raise 'not implemented yet'
+    def hash_for_key(key)
+      Digest::SHA1.hexdigest(key.to_s).hex
     end
   end
 end
--- spec/ch/ring_spec.rb
+++ spec/ch/ring_spec.rb
@@ -3,6 +3,12 @@ require "spec_helper"
 module ConsistentHashing
   describe Ring do
     it { should respond_to :add_node }
-    it { should respond_to :position_for_key }
+    it { should respond_to :hash_for_key }
+
+    describe '#hash_for_key' do
+      it 'generates different hash' do
+        subject.hash_for_key(1).should_not == subject.hash_for_key(2)
+      end
+    end
   end
 end
